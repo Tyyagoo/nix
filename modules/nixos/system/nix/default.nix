64496076f0b544e4d3ddf5053c9039843fc35bee
Tyyagoo <tyyago.dev@gmail.com>
2024-02-28 20:00:38 -0300

    add nix configuration

---
+++ modules/nixos/system/nix/default.nix
@@ -0,0 +1,44 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.nixty;
+let
+  cfg = config.system.nix;
+in {
+  options.system.nix = with types; {
+    enable = mkBoolOpt true "Enable nix configuration";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      nixfmt
+      nix-output-monitor
+    ];
+
+    nix = let
+      users = [ "root" config.user.name ];
+    in {
+      settings = {
+        experimental-features = "nix-command flakes";
+        http-connections = 50;
+        warn-dirty = false;
+        log-lines = 50;
+        # sandbox = "relaxed";
+        auto-optimise-store = true;
+        trusted-users = users;
+        allowed-users = users;
+      };
+
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than-30d";
+      };
+    };
+  };
+}
