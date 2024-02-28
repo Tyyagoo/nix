ecf865ae0b1b04d907803eb3ef6809f3cca385bb
Tyyagoo <tyyago.dev@gmail.com>
2024-02-28 19:23:06 -0300

    add timezone

---
+++ modules/nixos/system/time/default.nix
@@ -0,0 +1,20 @@
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.nixty;
+let
+  cfg = config.system.time;
+in {
+  options.system.time = with types; {
+    enable = mkBoolOpt true "Enable timezone configuration";
+  };
+
+  config = mkIf cfg.enable {
+    time.timeZone = "America/Sao_Paulo";
+  };
+}
