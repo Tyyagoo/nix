4f9ef90239164045b5d107dffa847c58b92ce12e
Tyyagoo <tyyago.dev@gmail.com>
2024-02-29 12:35:47 -0300

    add gpg

---
+++ modules/nixos/security/gpg/default.nix
@@ -0,0 +1,23 @@
+{ options, config, lib, pkgs, ... }:
+with lib;
+with lib.nixty;
+let cfg = config.security.gpg;
+in {
+  options.security.gpg.enable = mkBoolOpt' false;
+
+  config = mkIf cfg.enable {
+    programs = {
+      ssh.startAgent = false;
+      gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+        pinentryFlavor = "curses";
+      };
+    };
+
+    environment.systemPackages = with pkgs; [
+      gnupg
+      pinentry-curses
+    ];
+  };
+}
